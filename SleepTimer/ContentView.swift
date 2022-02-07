a9899331384de07756e06d3544831eed839c1c0c
Snow Lukin <everyoursden@gmail.com>
2022-02-07 15:57:16 +0300

    Initial Commit

---
+++ SleepTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SleepTimer
+//
+//  Created by Snow Lukin on 07.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SleepTimer/SleepTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTimerApp.swift
+//  SleepTimer
+//
+//  Created by Snow Lukin on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SleepTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
